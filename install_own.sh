28bf4efb15025c54e8b39b0cc92d57805681561b
robbert <robbert@localhost.localdomain>
2022-08-17 22:31:02 +0700

    update install_own.sh

--- install_own.sh
+++ install_own.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m'
+printf ${RED}"Just run this if you want to use my own dotfiles\nContinue?\n"${NC}
+read a
 rsync -avxHAXP --exclude-from=- own/. ~/ << "EXCLUDE"
 own
 EXCLUDE
