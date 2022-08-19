1078a479c29381bae674b881fef72acdc5ff013b
robbert <robbert@localhost.localdomain>
2022-08-19 21:23:53 +0700

    add Symbola font

--- install_own.sh
+++ install_own.sh
@@ -3,6 +3,7 @@ RED='\033[0;31m'
 NC='\033[0m'
 printf ${RED}"Just run this if you want to use my own dotfiles\nContinue?\n"${NC}
 read a
+sudo apt install fonts-symbola
 rsync -avxHAXP --exclude-from=- own/. ~/ << "EXCLUDE"
 own
 EXCLUDE
